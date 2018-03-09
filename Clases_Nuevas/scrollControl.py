36c4c12367ab786e1998cd043b7344bea12e8540
anton <anton.cid.mejias@udc.es>
2018-03-09 20:31:19 +0100

    Añadido módulo para el control del scroll "scrollControl". Los valores de scroll serán positivos ambos, para usarlo  en el código de prueba (además de hacer los cambios necesarios en fase) cambiar la función de actualizar pantalla de MiSprite cambiando la resta del scrolly a suma.

---
+++ Clases_Nuevas/scrollControl.py
@@ -0,0 +1,132 @@
+# -*- encoding: utf-8 -*-
+
+
+# -------------------------------------------------
+# Class used for the window scroll control
+# The values of the scroll will be positive,
+# so the changes for the sprite positions in the screen will be:
+# x - scroll[0] and y + scroll[1]
+class scrollControl:
+
+    # Input:
+    # scroll: Value of the initial scroll, both "x" and "y" will be positive
+    # minX, maxX, minY, maxY: Window edges for the scroll
+    # wHeight, wWidth: height and width of the scene screen
+    # scenery: Class that contains the scenary image
+    def __init__(self, scroll, minX, maxX, minY, maxY, wHeight, wWidth,scenery):
+        self.scroll = scroll
+        self.minX = minX
+        self.maxX = maxX
+        self.minY = minY
+        self.maxY = maxY
+        self.wHeight = wHeight
+        self.wWidth = wWidth
+        self.scenery =scenery
+
+    # Input:
+    # player: the charecter controlled by the player
+    #
+    # Output:
+    # True: It updated scroll
+    # False: It did not update the scroll
+    def updateScrollX(self, player):
+        # If the player overpass the left edge
+        if (player.rect.left < self.minX):
+            displacement = self.minX - player.rect.left
+
+            # If there is no more scenary in the left
+            if self.scroll[0] <= 0:
+                self.scroll = (0, self.scroll[1])
+                #TODO: establecer posicion?
+                player.establecerPosicion((self.minX, player.posicion[1]))
+
+                return False
+
+            # If it is possible to scroll left
+            else:
+                self.scroll = (self.scroll[0] - displacement, self.scroll[1])
+                return True
+
+        # If the player overpass the right edge
+        if (player.rect.right > self.maxX):
+            displacement = player.rect.right - self.maxX
+
+            # If there is no more scenary in the right
+            if self.scroll[0] + self.wWidth >= self.scenery.rect.right:
+                self.scroll = (self.scenery.rect.right - self.wWidth, self.scroll[1])
+                player.establecerPosicion(
+                    (self.scroll[0] + self.maxX - player.rect.width, player.posicion[1]))
+
+                return False
+
+            # If it is possible to scroll right
+            else:
+                self.scroll = (self.scroll[0] + displacement, self.scroll[1])
+                return True
+
+        # If the player is between the edges
+        return False
+
+    # Input:
+    # player: the charecter controlled by the player
+    #
+    # Output:
+    # True: It updated scroll
+    # False: It did not update the scroll
+    def updateScrollY(self, player):
+        # If the player overpass the top edge
+        if (player.rect.top < self.minY):
+            print("top")
+            displacement = self.minY - player.rect.top
+
+            # If there is no more scenary on the top
+            if self.scroll[1] + self.wHeight >= self.scenery.rect.bottom:
+                print("No top")
+                self.scroll = (self.scroll[0], self.scenery.rect.bottom - self.wHeight)
+                return False
+
+            # If it is possible to scroll top
+            else:
+                print("Scrolled top")
+                self.scroll = (self.scroll[0], self.scroll[1] + displacement)
+                return True
+
+        # If the player overpass the bottom edge
+        if (player.rect.bottom > self.maxY):
+            print("bottom")
+            displacement = player.rect.bottom - self.maxY
+
+            # If there is no more scenary on the bottom
+            if self.scroll[1] <= 0:
+                print("No sc")
+                self.scroll = (self.scroll[0], 0)
+                return False
+
+            # If it is possible to scroll bottom
+            else:
+                print("Scrolled bott")
+                self.scroll = (self.scroll[0], self.scroll[1] - displacement)
+                return True
+
+        # If the player is between the edges
+        return False
+
+    # Input:
+    # player: the charecter controled by the player
+    # sprites: The group of all the sprites that have to be updated
+    def updateScroll(self, player, sprites):
+
+        updatedScrollX = self.updateScrollX(player)
+        updatedScrollY = self.updateScrollY(player)
+        if updatedScrollX or updatedScrollY:
+            # Update the sprite positions with the new scroll
+            for sprite in iter(sprites):
+                sprite.establecerPosicionPantalla(self.scroll)
+
+            # Update the scenery to show the new position
+            self.scenery.update(self.scroll)
+
+        return
+
+
+
