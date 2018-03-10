42ac4aa2205676b68e1ef35f2704661a695da5a3
pedro-rivero <pedrorivero19@gmail.com>
2018-03-10 19:47:18 +0100

    ResourcesManager, versión traducida de gestorDeRecursos. OJETE; ahora la carpeta que mira por defecto es "images" y no "imagenes"

---
+++ Clases_Nuevas/resourcesManager.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+import pygame, sys, os
+from pygame.locals import *
+
+
+# -------------------------------------------------
+# resourcesManager Class
+
+#We implement an empty class, with only class methods
+class ResourcesManager(object):
+    resources = {}
+
+    @classmethod
+    def LoadImage(cls, name, colorkey=None):
+        # If the name of the file is amongst the already loaded resources
+        if name in cls.resources:
+            # That resource is returned
+            return cls.resources[name]
+        # If it hasn't been loaded previously
+        else:
+            # The image is loaded signaling the folder it is in
+            fullname = os.path.join('images', name)
+            try:
+                image = pygame.image.load(fullname)
+            except pygame.error, message:
+                print 'Cannot load image:', fullname
+                raise SystemExit, message
+            image = image.convert()
+            if colorkey is not None:
+                if colorkey is -1:
+                    colorkey = image.get_at((0, 0))
+                image.set_colorkey(colorkey, RLEACCEL)
+            # It is stored
+            cls.resources[name] = image
+            # It is returned
+            return image
+
+    @classmethod
+    def LoadFileCoordinates(cls, name):
+        # If the name of the file is amongst the already loaded resources
+        if name in cls.resources:
+            # That resource is returned
+            return cls.resources[name]
+        # If it hasn't been loaded previously
+        else:
+            # The resource is loaded signaling the folder it is in
+            fullname = os.path.join('images', name)
+            pfile = open(fullname, 'r')
+            data = pfile.read()
+            pfile.close()
+            # It is stored
+            cls.resources[name] = data
+            # It is returned
+            return data
